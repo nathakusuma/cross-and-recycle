741ea3da57fd2fa8c883416d5209e3b174a6a135
JoshuaJoelaskoro <joshuajoelaskoro@gmail.com>
2025-11-25 11:26:32 +0700

    feat: create vite.config.js to set allowed hosts

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    allowedHosts: [
+      'giwnf7jbvc0d.share.zrok.io',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
+})
